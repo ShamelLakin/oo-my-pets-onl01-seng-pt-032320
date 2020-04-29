379c317f9f1cdd7d4ad3216509dcc279a5313879
Shamel Lakin <christlife4u@gmail.com>
2020-04-29 21:06:04 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,44 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end 
+  
+  def name 
+    @name
+  end
+  
+  def say_species
+    "I am a human."
+  end
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.count
+    self.all.count
+  end 
+  
+  def self.reset_all
+    self.all.clear
+  end
+  
+  def cats 
+    Cat.all.select{|cat| cat.owner == self}
+  end 
+  
+   def dogs 
+    Dog.all.select{|dog| dog.owner == self}
+  end 
+  
+  def buy_cat(cat)
+    cats << Cat.new(cat, self)
+  end 
+
 end
