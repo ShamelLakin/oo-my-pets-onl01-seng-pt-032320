798215c8ae6048aeed6ae763a2770eb0543ecff3
Shamel Lakin <christlife4u@gmail.com>
2020-04-29 20:40:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,31 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  @@all = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end 
+  
+  def name 
+    @name
+  end
+  
+  def say_species
+    "I am a human."
+  end
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.count
+    self.all.count
+  end 
+  
+  def self.reset_all
+    self.all.clear
+  end
+  
+
 end
