665a7673418cc83d017f547ced8db24209226216
Shamel Lakin <christlife4u@gmail.com>
2020-04-29 20:46:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
-  # code goes here
+  
+  attr_accessor :owner
+  attr_reader :name
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @@all << self
+  end 
+  
+  def name 
+    @name
+  end 
+  
+  def self.all 
+    @@all
+  end 
 end
